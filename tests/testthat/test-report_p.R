91fad2dad58c78049d860719b9fc0fa718e346f9
debruine <debruine@gmail.com>
2019-03-23 20:56:29 +0000

    added readme and tests

---
+++ tests/testthat/test-report_p.R
@@ -0,0 +1,20 @@
+context("report_p")
+
+testthat::test_that("errors", {
+  testthat::expect_error(
+    report_p(-1),
+    "p cannot be less than 0"
+  )
+
+  testthat::expect_error(
+    report_p(2),
+    "p cannot be greater than 1"
+  )
+})
+
+testthat::test_that("default values", {
+  testthat::expect_equal(
+    report_p(p = 0.0222),
+    "p = .022"
+  )
+})
